01862b74427eb38f1b66eb38c1eecf772cef5b41
kopylash-roman <kopylash.roman@lll.kpi.ua>
2021-05-12 01:37:03 +0300

    add: complete the base architecture

--- src/main/java/com/kpi/coursework/InvertedIndexBuilder.java
+++ src/main/java/com/kpi/coursework/InvertedIndexBuilder.java
@@ -5,8 +5,19 @@ import java.util.List;
 import java.util.Map;
 
 public class InvertedIndexBuilder {
-    public Map<String, List<String>> buildInvertedIndex(File[][] parts, int threadNum) {
-        //TODO: provide implementation
+    public Map<String, List<String>> buildInvertedIndex(File[][] parts, int threadsNum) {
+        if (threadsNum == 1) {
+            return sequentialBuild(parts);
+        } else {
+            return parallelBuild(parts, threadsNum);
+        }
+    }
+
+    private Map<String, List<String>> sequentialBuild(File[][] parts) {
+        return null;
+    }
+
+    private Map<String, List<String>> parallelBuild(File[][] parts, int threadsNum) {
         return null;
     }
 }
