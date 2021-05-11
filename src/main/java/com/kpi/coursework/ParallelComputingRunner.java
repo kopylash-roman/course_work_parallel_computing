e20f5ebcd275576c81f5549782e86379d7843da4
kopylash-roman <kopylash.roman@lll.kpi.ua>
2021-05-12 01:20:14 +0300

    add: implement user input (threads num)

--- src/main/java/com/kpi/coursework/ParallelComputingRunner.java
+++ src/main/java/com/kpi/coursework/ParallelComputingRunner.java
@@ -1,6 +1,7 @@
 package com.kpi.coursework;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class ParallelComputingRunner {
     public static final String SOURCE_FOLDER = "text_files";
@@ -18,5 +19,35 @@ public class ParallelComputingRunner {
         for (int i = 0; i < subFolders.length; i++) {
             parts[i] = subFolders[i].listFiles();
         }
+
+        int threadNum = getThreadsNumFromUser();
+        if (threadNum == -1) {
+            return;
+        }
+
+
+    }
+
+    private static int getThreadsNumFromUser() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the num of threads (from 1 to 100) or 0 to exit: ");
+
+        int threadsNum;
+        while (true) {
+            String userInput = scan.next();
+            if (userInput.matches("\\d+")) {
+                if (Integer.parseInt(userInput) >= 0 && Integer.parseInt(userInput) <= 100) {
+                    threadsNum = Integer.parseInt(userInput);
+                    break;
+                } else if (Integer.parseInt(userInput) == 0) {
+                    System.out.println("Exiting...");
+                    return -1;
+                }
+            }
+            System.out.println("The userInput is incorrect! Try one more time.");
+            System.out.println("(Enter the num of threads from 1 to 100 or 0 to exit)");
+        }
+
+        return threadsNum;
     }
 }
