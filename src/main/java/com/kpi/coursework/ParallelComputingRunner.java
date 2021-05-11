45cc5866ed520f6c1882b3643a9938e02566d5a0
kopylash-roman <kopylash.roman@lll.kpi.ua>
2021-05-12 01:18:36 +0300

    add: provide file preparation

--- src/main/java/com/kpi/coursework/ParallelComputingRunner.java
+++ src/main/java/com/kpi/coursework/ParallelComputingRunner.java
@@ -1,7 +1,22 @@
 package com.kpi.coursework;
 
+import java.io.File;
+
 public class ParallelComputingRunner {
+    public static final String SOURCE_FOLDER = "text_files";
+
     public static void main(String[] args) {
-        System.out.println("Course work");
+        File sourceFolder = new File(SOURCE_FOLDER);
+
+        if (!sourceFolder.isDirectory() || sourceFolder.listFiles().length == 0) {
+            System.out.println("There are no files for building inverted index!");
+            return;
+        }
+
+        File[] subFolders = sourceFolder.listFiles();
+        File[][] parts = new File[subFolders.length][];
+        for (int i = 0; i < subFolders.length; i++) {
+            parts[i] = subFolders[i].listFiles();
+        }
     }
 }
