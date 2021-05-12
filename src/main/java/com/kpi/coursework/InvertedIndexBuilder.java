aef39f45c4ff3c771a7e7d5db5800cf9b1c1646d
kopylash-roman <kopylash.roman@lll.kpi.ua>
2021-05-12 23:57:49 +0300

    add!: provide parallel index building

--- src/main/java/com/kpi/coursework/InvertedIndexBuilder.java
+++ src/main/java/com/kpi/coursework/InvertedIndexBuilder.java
@@ -28,6 +28,52 @@ public class InvertedIndexBuilder {
     }
 
     private Map<String, Queue<String>> parallelBuild(File[][] parts, int threadsNum) {
-        return null;
+        BuildTask[] buildTasks = new BuildTask[threadsNum];
+
+        List<Map<String, Queue<String>>> listOfIndexParts = new ArrayList<>(threadsNum);
+        for (int i = 0; i < threadsNum; i++) {
+            Map<String, Queue<String>> indexPart = new HashMap<>();
+            listOfIndexParts.add(indexPart);
+
+            int[][] bounds = new int[parts.length][2];
+            for(int j = 0; j < bounds.length; j++) {
+                bounds[j][0] = parts[j].length / threadsNum * i;
+                bounds[j][1] = i == threadsNum - 1 ? parts[j].length : parts[j].length / threadsNum * (i + 1);
+            }
+
+            buildTasks[i] = new BuildTask(parts, bounds, indexPart, i);
+            buildTasks[i].start();
+        }
+
+        try {
+            for (BuildTask task : buildTasks) {
+                task.join();
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        return mergeIndexParts(listOfIndexParts);
+    }
+
+    private Map<String, Queue<String>> mergeIndexParts(List<Map<String, Queue<String>>> indexParts) {
+        Map<String, Queue<String>> invertedIndex = new ConcurrentHashMap<>();
+
+        for (Map<String, Queue<String>> indexPart : indexParts) {
+            String[] wordsArray = new String[indexPart.keySet().size()];
+            indexPart.keySet().toArray(wordsArray);
+
+            for (String word : wordsArray) {
+                if (invertedIndex.containsKey(word)) {
+                    invertedIndex.get(word).addAll(indexPart.get(word));
+                } else {
+                    PriorityQueue<String> wordPositions = new PriorityQueue<>();
+                    wordPositions.addAll(indexPart.get(word));
+                    invertedIndex.put(word, wordPositions);
+                }
+            }
+        }
+
+        return invertedIndex;
     }
 }
